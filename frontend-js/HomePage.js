51873a099e6dfd00778600abc3989b0389a539a6
Ice Nattanon <153441912+icenattanon@users.noreply.github.com>
2023-12-15 20:48:44 +0000

    Update HomePage.js

--- frontend-js/HomePage.js
+++ frontend-js/HomePage.js
@@ -1,1 +1,36 @@
+// Sample users data
+const users = [
+  {
+    username: "john",
+    password: "abc123"
+  },
+  {
+    username: "jane", 
+    password: "password1234"
+  }
+];
 
+// Get login form
+const form = document.getElementById("login-form");
+
+// Submit event handler
+form.addEventListener("submit", (event) => {
+  
+  // Prevent default submit
+  event.preventDefault();
+  
+  // Get form fields
+  const username = document.getElementById("username").value; 
+  const password = document.getElementById("password").value;
+
+  // Validate credentials
+  const user = users.find(u => u.username === username && u.password === password);
+
+  if (user) {
+    alert("Login successful!");
+    // Do something like redirect
+  } else {
+    alert("Invalid credentials");
+  }
+  
+});
